c68fe20792bf3a27a200ca1def09286c93c13424
chelseyrios9 <chelseyrios9@gmail.com>
2024-01-24 15:04:42 +0000

    initial commit

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/admin',
+  reactStrictMode: false,
+  swcMinify: true,
+  env: {
+    // For Local Server
+    API_PROD_URL: "http://142.93.64.125:8000/api/",
+    // API_PROD_URL: "https://laravel.pixelstrap.net/fastkart/api/",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "142.93.64.125:8000",
+      },
+      {
+        protocol: "https",
+        hostname: "laravel.pixelstrap.net",
+      },
+    ],
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+};
+
+module.exports = nextConfig;
