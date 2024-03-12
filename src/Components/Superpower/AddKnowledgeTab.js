51c9b427a1c42326bc2624ddb7f4a7c70b06565a
chelseyrios9 <chelseyrios9@gmail.com>
2024-03-12 14:04:24 +0100

    added the selected list in the Knowledge tab

--- src/Components/Superpower/AddKnowledgeTab.js
+++ src/Components/Superpower/AddKnowledgeTab.js
@@ -1,17 +1,12 @@
-import React, { useContext, useEffect, useMemo, useState } from "react";
-import Link from "next/link";
+import React, { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
-import request from "../../Utils/AxiosUtils";
 import SettingContext from "../../Helper/SettingContext";
 import I18NextContext from "@/Helper/I18NextContext";
 import { useTranslation } from "@/app/i18n/client";
-import { prompt } from "../../Utils/AxiosUtils/API";
 import MultiSelectField from "../InputFields/MultiSelectField";
-import SelectField from "../InputFields/SelectField";
 import { Table, Row, Col } from "reactstrap";
 import Btn from "@/Elements/Buttons/Btn";
-import { RiPencilLine } from "react-icons/ri";
 import TableLoader from "../Table/TableLoader";
 import axios from "axios";
 
@@ -26,15 +21,6 @@ const AddKnowledgeTab = ({ values, setFieldValue, errors, updateId }) => {
     () => axios.get(`https://supermind-n396.onrender.com/partitions/1`),
     { refetchOnWindowFocus: false, select: (res) => res.data }
   );
-  //   const [prompts, setPrompts] = useState([]);
-  // Getting data from Cart API
-  //   const { data: promptData, isLoading: promptLoader } = useQuery(["prompt", values.prompts], () => request({ url: prompt }), { refetchOnWindowFocus: false, select: (res) => res?.data });
-
-  //   useEffect(() => {
-  //     if(!promptLoader && promptData) {
-  //         setPrompts(promptData.data)
-  //     }
-  //   }, [promptLoader, promptData])
 
   useEffect(() => {
     if (partitions && !isLoading) {
@@ -68,6 +54,25 @@ const AddKnowledgeTab = ({ values, setFieldValue, errors, updateId }) => {
             require="false"
             data={partitionOptions}
           />
+          {values?.always_knowledges.length > 0 && <Table id="table_id" className={`role-table refund-table all-package theme-table datatable-wrapper mb-5`}>
+            <TableLoader fetchStatus={isLoading} />
+                <thead>
+                    <tr>
+                        <th className="sm-width">No</th>
+                        <th style={{width: "180px"}}>Name</th>
+                        <th>Decscription</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {values?.always_knowledges.map((prompt_id, index) => (
+                        <tr key={`prompt_table_${index}`}>
+                            <td>{index + 1}</td>
+                            <td>{partitions?.filter(item => item.partition_name === prompt_id)[0]?.partition_name}</td>
+                            <td>{partitions?.filter(item => item.partition_name === prompt_id)[0]?.partition_description}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>}
           <MultiSelectField
             errors={errors}
             values={values}
@@ -76,33 +81,28 @@ const AddKnowledgeTab = ({ values, setFieldValue, errors, updateId }) => {
             require="false"
             data={partitionOptions}
           />
+          {values?.library_knowledges.length > 0 && <Table id="table_id" className={`role-table refund-table all-package theme-table datatable-wrapper`}>
+            <TableLoader fetchStatus={isLoading} />
+                <thead>
+                    <tr>
+                        <th className="sm-width">No</th>
+                        <th style={{width: "180px"}}>Name</th>
+                        <th>Decscription</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {values?.library_knowledges.map((prompt_id, index) => (
+                    <tr key={`prompt_table_${index}`}>
+                        <td>{index + 1}</td>
+                        <td>{partitions?.filter(item => item.partition_name === prompt_id)[0]?.partition_name}</td>
+                        <td>{partitions?.filter(item => item.partition_name === prompt_id)[0]?.partition_description}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </Table>}
         </Col>
       </Row>
-      {/* {values?.prompts.length > 0 && <Table id="table_id" className={`role-table refund-table all-package theme-table datatable-wrapper`}>
-        <TableLoader fetchStatus={promptLoader} />
-        <thead>
-          <tr>
-            <th className="sm-width">No</th>
-            <th style={{width: "140px"}}>Name</th>
-            <th>Prompt Text</th>
-            <th style={{width: "80px"}}>Edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {values?.prompts.map((prompt_id, index) => (
-            <tr key={`prompt_table_${index}`}>
-              <td>{index + 1}</td>
-              <td>{prompts?.filter(item => item.id === prompt_id)[0]?.name}</td>
-              <td>{prompts?.filter(item => item.id === prompt_id)[0]?.prompt_text}</td>
-              <td>
-                <Link href={`/${i18Lang}/${prompt}/update/${prompt_id}`}>
-                  <RiPencilLine />
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>} */}
+      
     </>
   );
 };
