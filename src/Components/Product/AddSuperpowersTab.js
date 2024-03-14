f4965e2485d33ee3392a336e954dd3ecb7130e10
chelseyrios9 <chelseyrios9@gmail.com>
2024-03-14 11:40:29 +0100

    updated the add superpower tab

--- src/Components/Product/AddSuperpowersTab.js
+++ src/Components/Product/AddSuperpowersTab.js
@@ -1,36 +1,14 @@
-import React, { useContext, useEffect, useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import request from "../../Utils/AxiosUtils";
-import { store } from "../../Utils/AxiosUtils/API";
-import SimpleInputField from "../InputFields/SimpleInputField";
-import SearchableSelectInput from "../InputFields/SearchableSelectInput";
-import CheckBoxField from "../InputFields/CheckBoxField";
-import DescriptionInput from "./DescriptionInput";
-import SettingContext from "../../Helper/SettingContext";
-import I18NextContext from "@/Helper/I18NextContext";
-import { useTranslation } from "@/app/i18n/client";
-import Btn from "@/Elements/Buttons/Btn";
-import { AddtoCartAPI } from "../../Utils/AxiosUtils/API";
-import { Row, Col } from "reactstrap";
-import CustomDropDown from "../CommonComponent/CustomDropDown/CustomDropDown";
 import MultiSelectField from "../InputFields/MultiSelectField";
+import { superpower } from "../../Utils/AxiosUtils/API";
 
 const AddSuperpowersTab = ({ values, setFieldValue, errors, updateId }) => {
-  const { i18Lang } = useContext(I18NextContext);
-  const { t } = useTranslation(i18Lang, 'common');
-  const { state } = useContext(SettingContext);
-  const [cartProducts, setCartProducts] = useState([]);
-  const [selectedSuperpower, setSelectedSuperpower] = useState("");
   // Getting data from Cart API
-  const { data: addToCartData, isLoading: addToCartLoader } = useQuery([AddtoCartAPI], () => request({ url: AddtoCartAPI }), { refetchOnWindowFocus: false, select: (res) => res?.data });
+  const { data: superpowers, isLoading: superpowerloading } = useQuery([superpower], () => request({ url: superpower, method: 'get' }), { refetchOnWindowFocus: false, select: (res) => res?.data });
 
-  useEffect(() => {
-    if(!addToCartLoader && addToCartData) {
-        setCartProducts(addToCartData.items)
-    }
-  }, [addToCartLoader])
-
-  const selectOptions = useMemo(() => (cartProducts.map(item => ({id: item.product.id, name: item.product.name}))), [cartProducts]);
+  const selectOptions = useMemo(() => (superpowers?.data.map(item => ({id: item.id, name: item.name}))), [superpowers]);
   return (
     <>
       {/* <div className="inline">
