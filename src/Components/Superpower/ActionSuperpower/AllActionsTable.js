4a8b36c3eadd11f6859d641698782820f88fe4cb
Bassam <bas.ham1012@gmail.com>
2024-04-18 06:00:24 -0400

    add option to delete action

--- src/Components/Superpower/ActionSuperpower/AllActionsTable.js
+++ src/Components/Superpower/ActionSuperpower/AllActionsTable.js
@@ -16,6 +16,7 @@ const AllActionsTable = ({ data, ...props }) => {
   const [openModel, setOpenModel] = useState(false);
   const [actionDetail, setActionDetail] = useState(null);
   const [actionDescription, setActionDescription] = useState("")
+  const [headerObjState, setHeaderObjState] = useState({})
 
   useEffect(() => {
     const storedRole = localStorage.getItem("role");
@@ -24,21 +25,25 @@ const AllActionsTable = ({ data, ...props }) => {
       setRole(parsedRole.name);
     }
   }, []);
-  const headerObj = {
-    checkBox: false,
-    isOption: false,
-    noEdit: false,
-    optionHead: { title: "Action", type: "View" },
-    column: [{ title: "Name", apiKey: "name", sorting: true, sortBy: "desc" }],
-    data: data || [],
-  };
-  headerObj.data.map((element) => (element.sale_price = element?.sale_price));
-
-  let pro = headerObj?.column?.filter((elem) => {
-    return role == "vendor" ? elem.title !== "Approved" : elem;
-  });
-  headerObj.column = headerObj ? pro : [];
-
+  
+  useEffect(() => {
+    const headerObj = {
+      checkBox: false,
+      isOption: false,
+      noEdit: false,
+      optionHead: { title: "Action", type: "View" },
+      column: [{ title: "Name", apiKey: "name", sorting: true, sortBy: "desc" }],
+      data: data || [],
+    };
+    headerObj.data.map((element) => (element.sale_price = element?.sale_price));
+  
+    let pro = headerObj?.column?.filter((elem) => {
+      return role == "vendor" ? elem.title !== "Approved" : elem;
+    });
+    headerObj.column = headerObj ? pro : [];
+    setHeaderObjState(headerObj)
+  }, [data])
+  
   const viewActionDetail = (data) => {
     setActionDetail(data);
     setActionDescription(data?.description)
@@ -47,12 +52,11 @@ const AllActionsTable = ({ data, ...props }) => {
 
   const toggleModal = () => setOpenModel((prev) => !prev);
 
-
   const {mutate: updateDescriptionMutate, isLoading: updateDescriptionLoading} = useMutation(async ({description, name}) => {
     const resp = await fetch("http://134.209.37.239:3010/updateNodeDescription", {
         method: "PATCH",
         headers: {
-        "Content-Type": "application/json",
+          "Content-Type": "application/json",
         },
 
         body: JSON.stringify({description, name})
@@ -64,24 +68,51 @@ const AllActionsTable = ({ data, ...props }) => {
     throw respJson.message
   }, { refetchOnWindowFocus: false, select: (data) => data.data });
 
-  if (!data) return <Loader />;
+  const {mutate: deleteDescriptionMutate, isLoading: deleteDescriptionLoading} = useMutation(async ({workflowId}) => {
+    const resp = await fetch(`http://134.209.37.239:3010/deleteWorkflow?workflowId=${workflowId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+    })
+    const respJson = await resp.json()
+    if(respJson.success) {
+      setHeaderObjState(prev => {
+        prev.data = prev.data.filter((d) => d.workflow_id !== workflowId)
+        console.log(prev.data)
+        return prev
+      })
+      setOpenModel(false) 
+    }
+    throw respJson.message
+  }, { refetchOnWindowFocus: false, select: (data) => data.data });
+
+  if (!data || Object.keys(headerObjState).length <= 0) return <Loader />;
   return (
     <>
       <ShowTable
         {...props}
-        headerData={headerObj}
+        headerData={headerObjState}
         redirectLink={viewActionDetail}
       />
       <Modal fullscreen isOpen={openModel} toggle={toggleModal}>
         <ModalHeader toggle={toggleModal}>{actionDetail?.name}</ModalHeader>
         <ModalBody>
           <textarea style={{width:"100%", height: "100%"}} value={actionDescription} onChange={(e) => setActionDescription(e.target.value)} />
-          <Btn
-            title="Update Description"
-            className="align-items-center btn-theme add-button"
-            loading={updateDescriptionLoading}
-            onClick={() => updateDescriptionMutate({description: actionDescription, name: actionDetail?.name})}
-          />
+          <div style={{marginBottom: 10, display: "flex", justifyContent: "space-around"}}>
+            <Btn
+              title="Update Description"
+              className="align-items-center btn-theme add-button"
+              loading={updateDescriptionLoading || deleteDescriptionLoading}
+              onClick={() => updateDescriptionMutate({description: actionDescription, name: actionDetail?.name})}
+            />
+            <Btn
+              title="Delete Action"
+              className="align-items-center"
+              loading={updateDescriptionLoading || deleteDescriptionLoading}
+              onClick={() => deleteDescriptionMutate({workflowId: actionDetail?.workflow_id})}
+            />
+          </div>
         </ModalBody>
       </Modal>
     </>
