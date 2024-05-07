8d8db4b0302f5bc39a31a76f17c5964f5727b3e4
Bassam <bas.ham1012@gmail.com>
2024-05-07 02:09:07 -0400

    fix updateProcedure

--- src/Helper/ReactFlowChart.js
+++ src/Helper/ReactFlowChart.js
@@ -477,7 +477,7 @@ const ReactFlowChart = ({name, procedure, description, vectorQuery, procedureId,
                 title="Save Changes"
                 className="align-items-center"
                 loading={updateProcedureLoading}
-                onClick={() => updateProcedureMutate({procedure})}
+                onClick={() => updateProcedureMutate({procedure: stateProcedure})}
             />
         </div>
         <Modal fullscreen isOpen={openModel} toggle={toggleModal}>
