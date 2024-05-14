e01968cd61a005ea7a505d51f59ba0085cae4609
chelseyrios9 <chelseyrios9@gmail.com>
2024-05-14 18:59:16 +0200

    added the anthropic model header

--- src/Utils/OpenAIStream/index.js
+++ src/Utils/OpenAIStream/index.js
@@ -17,7 +17,9 @@ export const OpenAIStream = async (messages, model, api, api_key) => {
             headers: {
                 "Content-Type": "application/json",
                 Authorization: model.includes("claude") ? undefined : `Bearer ${api_key}`,
-                'x-api-key': model.includes("claude") ? api_key : undefined
+                'x-api-key': model.includes("claude") ? api_key : undefined,
+                'anthropic-version': model.includes("claude") ? '2023-06-01' : undefined, 
+                'anthropic-beta': model.includes("claude") ? 'messages-2023-12-15' : undefined,
             }
           }).then((response) => {
                 resolve(response.data.choices[0].message.content.trim());
